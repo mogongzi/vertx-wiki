51f866733242bddcd38f889a49635b357c9874ae
mogongzi <superrenyuan@hotmail.com>
2020-07-26 18:53:02 +0800

    add vert.x jdbc connection

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,8 +1,13 @@
 package io.vertx.starter;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -12,6 +17,9 @@ public class MainVerticle extends AbstractVerticle {
   private static final String SQL_SAVE_PAGE = "update Pages set Content=? where Id=?";
   private static final String SQL_ALL_PAGES = "select Name from Pages";
   private static final String SQL_DELETE_PAGE = "delete from Pages where Id=?";
+
+  private JDBCClient dbClient;
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   @Override
   public void start(Promise<Void> promise) {
@@ -27,6 +35,30 @@ public class MainVerticle extends AbstractVerticle {
 
   private Future<Void> prepareDatabase() {
     Promise<Void> promise = Promise.promise();
+
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+    .put("url", "jdbc:hsqldb:file:db/wiki")
+    .put("driver_class", "org.hsqldb.jdbcDriver")
+    .put("max_pool_size", 30));
+
+    dbClient.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        promise.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          connection.close();
+          if (create.failed()) {
+            LOGGER.error("Database preparation error", create.cause());
+            promise.fail(create.cause());
+          } else {
+            promise.complete();
+          }
+        });
+      }
+    });
+
     return promise.future();
   }
 
